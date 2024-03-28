275c44014f70f7002c93059cdce414c14db854d1
Claudio Poblete <pobleteclaudio2103@gmail.com>
2024-03-27 23:53:56 -0300

    se configura funciones incrementar y disminucion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,35 @@
 let precio = 400000
 
-let precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio;
+let precioSpan = document.querySelector(".precio-inicial")
+precioSpan.innerHTML = precio
 
-const total = document.querySelector(".cantidad");
-let count = 0;
+const valorSpan = document.querySelector(".valor-total")
+const total = document.querySelector(".cantidad")
+let count = 0
+// const sumar = document.querySelector('#sumar')
+// const restar = document.querySelector('#restar')
+
+// sumar.addEventListener('click', () =>{
+//   total.innerHTML = +total.innerHTML + 1
+//   valorSpan.innerHTML =  total.innerHTML * precio
+// })
+
+// restar.addEventListener('click', () =>{
+//   total.innerHTML = +total.innerHTML - 1
+//   valorSpan.innerHTML =  total.innerHTML * precio
+// })
 
 const incremento = () =>{
-  count++;
-  total.innerHTML = count;
-  let resultado = precio * count;
-  let valorSpan = document.querySelector(".valor-total");
-  valorSpan.innerHTML = resultado;
+  count++
+  total.innerHTML = count
+  valorSpan.innerHTML =  total.innerHTML * precio
 }
 
 const disminucion = () =>{
-  count--;
-  total.innerHTML = count;
-  let resultado = precio * count;
-  let valorSpan = document.querySelector(".valor-total");
-  valorSpan.innerHTML = resultado;
+  if (total.innerHTML >= 1){
+    count--
+    total.innerHTML = count
+    valorSpan.innerHTML =  total.innerHTML * precio
+  }
 }
 
